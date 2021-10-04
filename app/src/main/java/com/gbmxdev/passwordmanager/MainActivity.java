4a89d3e0ccc906cba1cd64942a9e57646bbe714f
gbmillion <gbmillion@bucs.fsw.edu>
2021-10-04 10:08:53 -0400

    set up a todo list ;)

---
+++ app/src/main/java/com/gbmxdev/passwordmanager/MainActivity.java
@@ -0,0 +1,99 @@
+package com.gbmxdev.passwordmanager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.gbmxdev.passwordmanager.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+/*
+TODO:
+    set up file stream
+    encryption of file
+    store user_id:pass combos in CSV format
+    store domain/site
+
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Email George@gbmxdev.com what you think!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {//just set the duc king preference instead of using another activity
+            SharedPreferences encryptPref = this.getPreferences(Context.MODE_PRIVATE);
+            boolean defaultValue = getResources().getBoolean(R.bool.encrypt);
+            defaultValue=encryptPref.getBoolean("encrypt", false);
+            if (defaultValue){
+                SharedPreferences.Editor editor = encryptPref.edit();
+                editor.putBoolean(getString(R.string.encrypt), true);
+                editor.apply();
+            } else {
+                SharedPreferences.Editor editor = encryptPref.edit();
+                editor.putBoolean(getString(R.string.encrypt), false);
+                editor.apply();
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
